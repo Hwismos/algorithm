2936ab55189102db0a4fda8cddec3e224d00ec86
Hwiriric <smk6270@gmail.com>
2023-01-15 17:57:04 +0900

    rotate와 check 함수를 직접 채워봄, 이전에 너무 어렵게 생각

--- Implementation/자물쇠와 열쇠.py
+++ Implementation/자물쇠와 열쇠.py
@@ -1,108 +1,48 @@
-# 고려 사항
-# 경계 지점 고려
-
-def print_matrix(data):
-    for line in data:
-        print(line)
-    print('\n')
-
-
-def make_lock_bigger(key, lock):
-    m = len(key)
-    n = len(lock)
-    x = n + ((m - 1) * 2)
-    big_lock = [[0] * x for _ in range(x)]
-
-    for i in range(n):
-        for j in range(n):
-            big_lock[i + m - 1][j + m - 1] = lock[i][j]
-    return big_lock
-
-
-def copy_matrix(_from, _to):
-    for i in range(len(_from)):
-        for j in range(len(_from[i])):
-            _from[i][j] = _to[i][j]
-
-
-def is_match(key, big_lock):
-    m = len(key)
-    n = len(big_lock)
-    x = n - ((m - 1) * 2)
-    copied_big_lock = [[0] * n for _ in range(n)]
-    check_lock = [[0] * x for _ in range(x)]
-
-    copy_matrix(copied_big_lock, big_lock)
-
-    for i in range(n - m + 1):
-        for j in range(n - m + 1):
-            for x in range(m):
-                for y in range(m):
-                    copied_big_lock[x + i][y + j] += key[x][y]
-            for x in range(m):
-                for y in range(m):
-                    check_lock[x][y] = copied_big_lock[x + m - 1][y + m - 1]
-            for each_line in check_lock:
-                if not(all(each_line)):
-                    break
-            else:
-                return True
-            copy_matrix(copied_big_lock, big_lock)
-    return False
-
-
 def rotate_key(key):
-    """
-    (0, 0) -> (0, 2)
-    (0, 1) -> (1, 2)
-    (0, 2) -> (2, 2)
-    (1, 0) -> (0, 1)
-    (1, 1) -> (1, 1)
-    (1, 2) -> (2, 1)
-    (2, 0) -> (0, 0)
-    (2, 1) -> (1, 0)
-    (2, 2) -> (2, 0)
-    """
     m = len(key)
     rotated_key = [[0] * m for _ in range(m)]
-
     for i in range(m):
         for j in range(m):
-            rotated_key[i][j] = key[m - j - 1][i]
+            rotated_key[j][m - i - 1] = key[i][j]
     return rotated_key
 
 
-def is_lock_one(lock):
-    for each_line in lock:
-        if not (all(each_line)):
-            return False
-    else:
-        return True
+def check(new_lock):
+    m = len(new_lock) // 3
+
+    for i in range(m, 2*m):
+        for j in range(m, 2*m):
+            if new_lock[i][j] != 1:
+                return False
+    return True
 
 
 def solution(key, lock):
-    if is_lock_one(lock):
-        return True
+    n = len(lock)
+    m = len(key)
 
-    big_lock = make_lock_bigger(key, lock)
+    # 자물쇠 크기를 기존은 3배로 변환
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
 
-    for i in range(4):
-        print_matrix(key)
-        if is_match(key, big_lock):
-            return True
+    # 새로운 자물쇠에 기존 자물쇠 내용 삽입
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
+
+    # 4가지 방향에 대해서 확인
+    for rotation in range(4):
         key = rotate_key(key)
+        for x in range(n * 2):
+            for y in range(n * 2):
+                # 자물쇠에 열쇠 삽입
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                # 새 자물쇠를 이용해 열쇠가 정확한지를 검사
+                if check(new_lock):
+                    return True
+                # 새 자물쇠에서 열쇠 빼기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
     return False
-
-
-def main():
-    key = [[0, 0, 0], [1, 1, 0], [0, 1, 1]]
-    lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-    # lock = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
-    if solution(key, lock):
-        print('True')
-    else:
-        print('False')
-
-
-if __name__ == "__main__":
-    main()
